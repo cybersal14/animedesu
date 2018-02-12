3fcac955c4da3b5bc0f0b44f1f62ae235c940554
cybersal14 <muhamadfaisal288@gmail.com>
2018-02-13 06:08:15 +0700

    Add files via upload

---
+++ lol.js
@@ -0,0 +1,11 @@
+<img src="https://3.bp.blogspot.com/-04oChnXIv7E/WnrNa5TBX9I/AAAAAAAAASc/8ZSj6L8MrQsOOB1SLd6L3OTKEYzd_vG-ACPcBGAYYCw/s1600/86794l.jpg" width="50"height="50"style="float:left" /> Teks postingan 
+
+
+
+<img style="padding:3px;border:6px solid #ea6153; background:#fafafa;" src="https://3.bp.blogspot.com/-04oChnXIv7E/WnrNa5TBX9I/AAAAAAAAASc/8ZSj6L8MrQsOOB1SLd6L3OTKEYzd_vG-ACPcBGAYYCw/s1600/86794l.jpg"/>
+
+
+<img style="border: 5px outset #3498DB; background-color:#ECF0F1; padding: 5px; width: 300px;" src="https://3.bp.blogspot.com/-04oChnXIv7E/WnrNa5TBX9I/AAAAAAAAASc/8ZSj6L8MrQsOOB1SLd6L3OTKEYzd_vG-ACPcBGAYYCw/s1600/86794l.jpg>
+
+
+<img style="padding:3px;border:6px solid #ea6153; background:#fafafa;" src="https://3.bp.blogspot.com/-04oChnXIv7E/WnrNa5TBX9I/AAAAAAAAASc/8ZSj6L8MrQsOOB1SLd6L3OTKEYzd_vG-ACPcBGAYYCw/s1600/86794l.jpg" style="float: left;" width="50" /> Teks postingan
